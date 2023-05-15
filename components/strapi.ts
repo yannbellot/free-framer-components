254acc1139d78e00931983f523c2856130de06f3
Yann Bellot <ybellot@gmail.com>
2023-05-15 16:20:00 +0200

    Update strapi.ts

--- components/strapi.ts
+++ components/strapi.ts
@@ -1,6 +1,6 @@
 /*
 
-Strapi for Framer
+Weather for Framer
 MIT License
 
 // The MIT License
@@ -30,16 +30,25 @@ THE SOFTWARE.
 import React from "react"
 import useFetch from "react-fetch-hook"
 import { addPropertyControls, ControlType } from "framer"
-import {
-    Icon,
-    iconKeys,
-} from "https://framer.com/m/Feather-H6aU.js@0pLhZgteIMGONoge6j70"
 
-export default function Strapi(props) {
-    const apiURL =
-        props.apiUrl + "/api/" + props.collection + "/" + props.entry + "/"
-    const imageURL = props.apiUrl + "/uploads/" + props.imageName
-    const { isLoading, error, data } = useFetch(apiURL)
+export default function Weather(props) {
+    let OwKey = props.OwKey
+    let lat = props.lat
+    let long = props.long
+    let unit = props.unit
+    let tempUnitSymbol
+    let speedUnit
+    let speedUnitSymbol
+    if (props.unit == "metric") {
+        tempUnitSymbol = "°C"
+        speedUnit = 3.6
+        speedUnitSymbol = "km/h"
+    }
+    if (props.unit == "imperial") {
+        tempUnitSymbol = "°F"
+        speedUnit = 3.3
+        speedUnitSymbol = "mph"
+    }
     const errorStyles = {
         background: "#EEEEEE",
         color: "rgb(255, 98, 26)",
@@ -64,7 +73,53 @@ export default function Strapi(props) {
         margin: "0",
         fontWeight: "bold",
     }
-    const contentStyles = {
+    const { isLoading, error, data } = useFetch(
+        "https://api.openweathermap.org/data/2.5/weather?lat=" +
+            lat +
+            "&lon=" +
+            long +
+            "&lang=" +
+            props.lang +
+            "&units=" +
+            unit +
+            "&appid=" +
+            OwKey
+    )
+    if (isLoading) return "Loading..."
+    if (error)
+        return (
+            <div style={messageStyles}>
+                <p style={errorStyles}>Loading error!</p>
+                <p>
+                    To display weather, please enter valid OpenWeather 2.5 API
+                    key.
+                </p>
+            </div>
+        )
+
+    let date = new Date()
+    let dateNow = Date.now()
+
+    let sunrise = new Date(data.sys.sunrise * 1000)
+    let sunriseHour = sunrise.getHours()
+    let sunriseMinutes = sunrise.getMinutes()
+    let sunset = new Date(data.sys.sunset * 1000)
+    let sunsetHour = sunset.getHours()
+    let sunsetMinutes = sunset.getMinutes()
+    let windSpeed = data.wind.speed * speedUnit * 100
+    windSpeed = Math.round(windSpeed)
+    windSpeed = windSpeed / 100
+
+    let iconSize = props.icon.iconSize
+
+    let weatherIcon =
+        "https://openweathermap.org/img/wn/" +
+        data.weather[0].icon +
+        iconSize +
+        ".png"
+    const textStyles = {
+        padding: "0px",
+        margin: "0px",
         fontFamily: props.font,
         fontSize: props.fontSize,
         fontWeight: props.fontWeight,
@@ -72,152 +127,325 @@ export default function Strapi(props) {
         textAlign: props.textAlign,
         letterSpacing: props.letterSpacing,
         lineHeight: props.lineHeight,
-        padding: "0",
-        margin: "0",
+        textTransform: props.transform,
     }
 
-    if (isLoading) return <p style={loadingStyles}>Loading...</p>
-    if (error)
-        if (props.type === true) {
+    let iconMargin = props.icon.iconMargin
+
+    if (props.dataType === "w") {
+        if (props.icon.iconPosition === "L")
             return (
-                <div style={messageStyles}>
-                    <p style={errorStyles}>Loading error!</p>
-                    <p>
-                        To display a resource, please enter valid API URL,
-                        Collection & Field.
-                    </p>
+                <div style={{ display: "flex", alignItems: "center" }}>
+                    <img
+                        src={weatherIcon}
+                        style={{
+                            display: "block",
+                            marginRight: iconMargin,
+                        }}
+                    />
+                    <p style={textStyles}>{data.weather[0].description}</p>
                 </div>
             )
-        } else {
+        if (props.icon.iconPosition === "T")
             return (
-                <div style={messageStyles}>
-                    <p style={errorStyles}>Loading error!</p>
-                    <p>
-                        To display an image, please enter valid API URL & File
-                        name.
-                    </p>
+                <div style={{ textAlign: "center" }}>
+                    <img
+                        src={weatherIcon}
+                        style={{
+                            display: "block",
+                            marginBottom: iconMargin,
+                            marginLeft: "auto",
+                            marginRight: "auto",
+                        }}
+                    />
+                    <p style={textStyles}>{data.weather[0].description}</p>
                 </div>
             )
-        }
-    const field = eval("data.data.attributes." + props.field)
-    if (data) {
-        if (props.type === true) {
-            if (props.tag == "h1") {
-                return (
-                    <div>
-                        <h1 style={contentStyles}>{field}</h1>
-                    </div>
-                )
-            }
-            if (props.tag == "h2") {
-                return (
-                    <div>
-                        <h2 style={contentStyles}>{field}</h2>
-                    </div>
-                )
-            }
-            if (props.tag == "h3") {
-                return (
-                    <div>
-                        <h3 style={contentStyles}>{field}</h3>
-                    </div>
-                )
-            }
-            if (props.tag == "h4") {
-                return (
-                    <div>
-                        <h4 style={contentStyles}>{field}</h4>
-                    </div>
-                )
-            }
-            if (props.tag == "h5") {
-                return (
-                    <div>
-                        <h5 style={contentStyles}>{field}</h5>
-                    </div>
-                )
-            }
-            if (props.tag == "h6") {
-                return (
-                    <div>
-                        <h6 style={contentStyles}>{field}</h6>
-                    </div>
-                )
-            }
-            if (props.tag == "p") {
-                return (
-                    <div>
-                        <p style={contentStyles}>{field}</p>
-                    </div>
-                )
-            }
-        }
-        if (props.type === false) {
-            return <img src={imageURL} />
-        }
+        if (props.icon.iconPosition === "R")
+            return (
+                <div style={{ display: "flex", alignItems: "center" }}>
+                    <p style={textStyles}>{data.weather[0].description}</p>
+                    <img
+                        src={weatherIcon}
+                        style={{
+                            display: "block",
+                            marginLeft: iconMargin,
+                        }}
+                    />
+                </div>
+            )
+        if (props.icon.iconPosition === "B")
+            return (
+                <div style={{ textAlign: "center" }}>
+                    <p style={textStyles}>{data.weather[0].description}</p>
+                    <img
+                        src={weatherIcon}
+                        style={{
+                            display: "block",
+                            marginTop: iconMargin,
+                            marginLeft: "auto",
+                            marginRight: "auto",
+                        }}
+                    />
+                </div>
+            )
     }
+    if (props.dataType === "t")
+        return <p style={textStyles}>{data.main.temp + tempUnitSymbol}</p>
+    if (props.dataType === "tmin")
+        return <p style={textStyles}>{data.main.temp_min + tempUnitSymbol}</p>
+    if (props.dataType === "tmax")
+        return <p style={textStyles}>{data.main.temp_max + tempUnitSymbol}</p>
+    if (props.dataType === "tfl")
+        return <p style={textStyles}>{data.main.feels_like + tempUnitSymbol}</p>
+    if (props.dataType === "p")
+        return <p style={textStyles}>{data.main.pressure}hPa</p>
+    if (props.dataType === "h")
+        return <p style={textStyles}>{data.main.humidity}%</p>
+    if (props.dataType === "v")
+        return <p style={textStyles}>{data.visibility} m</p>
+    if (props.dataType === "ws")
+        return <p style={textStyles}>{windSpeed + " " + speedUnitSymbol}</p>
+    if (props.dataType === "wd")
+        return <p style={textStyles}>{data.wind.deg}°</p>
+    if (props.dataType === "sr")
+        return (
+            <p style={textStyles}>
+                {sunriseHour}:{sunriseMinutes}
+            </p>
+        )
+    if (props.dataType === "ss")
+        return (
+            <p style={textStyles}>
+                {sunsetHour}:{sunsetMinutes}
+            </p>
+        )
+    if (props.dataType === "c") return <p style={textStyles}>{data.name}</p>
 }
 
-addPropertyControls(Strapi, {
-    apiUrl: {
+addPropertyControls(Weather, {
+    OwKey: {
         type: ControlType.String,
-        title: "URL",
+        title: "API key",
         defaultValue: "",
-        placeholder: "Your API URL",
+        placeholder: "Your OpenWeather key",
         description:
-            "[Strapi documentation](https://docs.strapi.io/dev-docs/intro)",
+            "[View OpenWeather 2.5](https://openweathermap.org/current) or [Create an API Key](https://home.openweathermap.org/users/sign_in)",
     },
-    type: {
-        type: ControlType.Boolean,
+    lat: {
+        type: ControlType.String,
+        defaultValue: "48.8566",
+        placeholder: "Latitude",
+    },
+    long: {
+        type: ControlType.String,
+        defaultValue: "2.3522",
+        placeholder: "Longitude",
+    },
+    dataType: {
+        type: ControlType.Enum,
         title: "Type",
-        defaultValue: true,
-        enabledTitle: "Texte",
-        disabledTitle: "Image",
+        defaultValue: "x",
+        displaySegmentedControl: false,
+        segmentedControlDirection: "vertical",
+        options: [
+            "w",
+            "t",
+            "tmin",
+            "tmax",
+            "tfl",
+            "p",
+            "h",
+            "v",
+            "ws",
+            "wd",
+            "sr",
+            "ss",
+            "c",
+        ],
+        optionTitles: [
+            "Weather",
+            "Temp",
+            "Temp min",
+            "Temp max",
+            "Feel like",
+            "Pressure",
+            "Humidity",
+            "Visibility",
+            "Wind speed",
+            "Wind deg",
+            "Sunrise",
+            "Sunset",
+            "City",
+        ],
     },
-    collection: {
-        type: ControlType.String,
-        title: "Collection",
-        defaultValue: "",
-        placeholder: "Your collection name",
+    icon: {
+        type: ControlType.Object,
+        title: "Icon",
+        controls: {
+            iconSize: {
+                type: ControlType.Enum,
+                defaultValue: "",
+                title: "Icon",
+                displaySegmentedControl: true,
+                segmentedControlDirection: "horizontal",
+                options: ["", "@2x", "@4x"],
+                optionTitles: ["S", "M", "XL"],
+            },
+            iconPosition: {
+                type: ControlType.Enum,
+                title: "Position",
+                defaultValue: "L",
+                displaySegmentedControl: true,
+                segmentedControlDirection: "horizontal",
+                options: ["L", "T", "R", "B"],
+                optionTitles: ["L", "T", "R", "B"],
+            },
+            iconMargin: {
+                type: ControlType.Number,
+                title: "Margin",
+                defaultValue: 0,
+                step: 1,
+                displayStepper: true,
+            },
+        },
         hidden(props) {
-            return props.type === false
+            return props.dataType !== "w"
         },
     },
-    imageName: {
-        type: ControlType.String,
-        title: "File name",
-        defaultValue: "",
-        placeholder: "Your file name",
+
+    lang: {
+        type: ControlType.Enum,
+        title: "Language",
+        defaultValue: "en",
+        displaySegmentedControl: false,
+        segmentedControlDirection: "vertical",
+        options: [
+            "af",
+            "al",
+            "ar",
+            "az",
+            "bg",
+            "ca",
+            "cz",
+            "da",
+            "de",
+            "el",
+            "en",
+            "eu",
+            "fa",
+            "fi",
+            "fr",
+            "gl",
+            "he",
+            "hi",
+            "hr",
+            "hu",
+            "id",
+            "it",
+            "ja",
+            "kr",
+            "la",
+            "lt",
+            "mk",
+            "no",
+            "nl",
+            "pl",
+            "pt",
+            "pt_br",
+            "ro",
+            "ru",
+            "sv",
+            "sk",
+            "sl",
+            "sp",
+            "sr",
+            "th",
+            "tr",
+            "ua",
+            "vi",
+            "zh_cn",
+            "zh_tw",
+            "zu",
+        ],
+        optionTitles: [
+            "Afrikaans",
+            "Albanian",
+            "Arabic",
+            "Azerbaijani",
+            "Bulgarian",
+            "Catalan",
+            "Czech",
+            "Danish",
+            "German",
+            "Greek",
+            "English",
+            "Basque",
+            "Persian (Farsi)",
+            "Finnish",
+            "French",
+            "Galician",
+            "Hebrew",
+            "Hindi",
+            "Croatian",
+            "Hungarian",
+            "Indonesian",
+            "Italian",
+            "Japanese",
+            "Korean",
+            "Latvian",
+            "Lithuanian",
+            "Macedonian",
+            "Norwegian",
+            "Dutch",
+            "Polish",
+            "Portuguese",
+            "Português Brasil",
+            "Romanian",
+            "Russian",
+            "Swedish",
+            "Slovak",
+            "Slovenian",
+            "Spanish",
+            "Serbian",
+            "Thai",
+            "Turkish",
+            "Ukrainian",
+            "Vietnamese",
+            "Chinese Simplified",
+            "Chinese Traditional",
+            "Zulu",
+        ],
         hidden(props) {
-            return props.type === true
+            return props.dataType !== "w"
         },
     },
-    entry: {
-        type: ControlType.Number,
-        title: "Entry",
-        defaultValue: 1,
-        displayStepper: true,
-        min: 1,
+
+    unit: {
+        type: ControlType.Enum,
+        title: "Unit",
+        defaultValue: "metric",
+        displaySegmentedControl: true,
+        segmentedControlDirection: "horizontal",
+        options: ["metric", "imperial"],
+        optionTitles: ["metric", "imperial"],
         hidden(props) {
-            return props.type === false
-        },
-    },
-    field: {
-        type: ControlType.String,
-        title: "Field",
-        defaultValue: "",
-        placeholder: "Your field name",
-        hidden(props) {
-            return props.type === false
+            return (
+                props.dataType == "w" ||
+                props.dataType == "p" ||
+                props.dataType == "h" ||
+                props.dataType == "v" ||
+                props.dataType == "wd" ||
+                props.dataType == "sr" ||
+                props.dataType == "ss" ||
+                props.dataType == "c"
+            )
         },
     },
     font: {
         type: ControlType.String,
         placeholder: "Inter",
         defaultValue: "Inter",
-        hidden(props) {
-            return props.type === false
-        },
     },
     fontSize: {
         title: "Size",
@@ -227,37 +455,17 @@ addPropertyControls(Strapi, {
         step: 0.5,
         defaultValue: 16,
         displayStepper: true,
-        hidden(props) {
-            return props.type === false
-        },
     },
     fontWeight: {
         type: ControlType.Enum,
         options: [100, 200, 300, 400, 500, 600, 700, 800, 900],
-        defaultValue: 400,
+        defaultValue: 900,
         title: "Weight",
-        hidden(props) {
-            return props.type === false
-        },
     },
     color: {
         type: ControlType.Color,
         title: "Color",
         defaultValue: "black",
-        hidden(props) {
-            return props.type === false
-        },
-    },
-    textAlign: {
-        type: ControlType.Enum,
-        displaySegmentedControl: true,
-        title: "Align",
-        options: ["left", "center", "right"],
-        optionTitles: ["Left", "Center", "Right"],
-        defaultValue: "left",
-        hidden(props) {
-            return props.type === false
-        },
     },
     letterSpacing: {
         title: "Letter",
@@ -265,30 +473,14 @@ addPropertyControls(Strapi, {
         defaultValue: 0,
         step: 0.1,
         displayStepper: true,
-        hidden(props) {
-            return props.type === false
-        },
     },
-
-    lineHeight: {
-        type: ControlType.Number,
-        title: "Line",
-        min: 0,
-        max: 5,
-        displayStepper: true,
-        step: 0.1,
-        defaultValue: 1.3,
-        hidden(props) {
-            return props.type === false
-        },
-    },
-    tag: {
+    transform: {
         type: ControlType.Enum,
-        options: ["h1", "h2", "h3", "h4", "h5", "h6", "p"],
-        defaultValue: "p",
-        title: "Tag",
+        options: ["None", "Capitalize", "Uppercase", "Lowercase"],
+        defaultValue: "None",
+        title: "Transform",
         hidden(props) {
-            return props.type === false
+            return props.dataType !== "w" && props.dataType !== "c"
         },
     },
 })
